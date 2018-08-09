5168c8cf41b11b01e69b6b477bc39ffff15e05e3
Himanshu Goyal <himanshu.goyal@live.com>
2018-08-10 04:21:49 +0530

    Initial Commit

---
+++ DatingApp.API/Data/DataContext.cs
@@ -0,0 +1,18 @@
+using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Data
+{
+    //DbContext will a query to the database via entity framework.
+    //DataContext is a derived class of DbContext
+    public class DataContext : DbContext
+    {
+        /*$ dotnet ef migration add InitialCreate
+         */
+        //:base to chain the Constructor DataContext with DbContext
+        public DataContext(DbContextOptions<DataContext> options): base (options){}
+
+        //There is a convention to pluralise the entities
+        public DbSet<Value> Values { get; set; }
+    }
+}
---
+++ DatingApp.API/Models/Value.cs
@@ -0,0 +1,10 @@
+namespace DatingApp.API.Models
+{
+    public class Value
+    {
+        //Getters and Setters are needed to access these properties from other class of our application.
+        public int Id { get; set; } 
+
+        public string Name { get; set; }
+    }
+}
