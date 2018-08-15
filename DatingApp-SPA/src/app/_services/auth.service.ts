fc9f7c42708bd7af1ed03f39f0b23da224446bdd
Himanshu Goyal <himanshu.goyal@live.com>
2018-08-15 22:18:42 +0530

    Client Side login and register - Introduction to angular services

---
+++ DatingApp-SPA/src/app/_services/auth.service.ts
@@ -0,0 +1,33 @@
+// When a service is created we need to create this in our app module in th providers
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+// This allows us to inject things in our service
+// NOTE: Components are automatically injectable by default
+@Injectable({
+  // Provided In: This tells us which module is providing this service, in this case,  root module is providing the service.
+  providedIn: 'root'
+})
+export class AuthService {
+baseUrl = 'http://localhost:5000/api/auth/';
+
+constructor(private http: HttpClient) { }
+
+login (model: any) {
+  // We might need to specify the type of content, neither authorization header as of now.
+  // We need to add code to do some thing when it comes back from the server
+  // Inorder to do something we have an observable, rxJS operators
+  // .pipe allows us to chain rxJs operators
+  // take the response and do something
+  return this.http.post(this.baseUrl + 'login', model)
+  .pipe(
+    map((response: any) => {
+      const user = response;
+      if (user) {
+        localStorage.setItem('token', user.token);
+      }
+    })
+  );
+}
+}
