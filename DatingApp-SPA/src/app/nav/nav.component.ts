67ab6038de9d9d7417425861bac045ccc24ccf53
Himanshu Goyal <himanshu.goyal@live.com>
2018-08-15 22:37:13 +0530

    Client Side login and register | Injecting Angular services in our Comp

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
 
 @Component({
   selector: 'app-nav',
@@ -7,13 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
   model: any = {};
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
   }
 
   login() {
-    console.log(this.model);
+    // the service is returning observable we need to subscribe to observable
+   this.authService.login(this.model).subscribe(next => {
+     console.log('Logged in Successfully');
+   }, error => {
+     console.log('Failed ot login');
+   });
   }
 
   // NOTE: Need to include formsmodule in modules
