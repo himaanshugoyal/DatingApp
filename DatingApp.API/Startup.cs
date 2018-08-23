0a2eeae26d2021f997cbe2fce0cc2a828d4845ac
Himanshu Goyal <himanshu.goyal@live.com>
2018-08-24 05:20:40 +0530

    Creating the users controller

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -36,7 +36,11 @@ namespace DatingApp.API
         {
             
            services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1)
+            //The below is added handle the self referncing error that we get w.r.t. User.
+                .AddJsonOptions(opt => {
+                    opt.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+                });
             services.AddCors();
             services.AddTransient<Seed>();
             /*Note:
