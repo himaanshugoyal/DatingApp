1344541a12a00f0109b00dcafcdd6fc5debce8e1
Himanshu Goyal <himanshu.goyal@live.com>
2019-08-10 14:01:13 +0530

    Creating Member Cards to display on our Member list page

--- DatingApp-SPA/src/app/routes.ts
+++ DatingApp-SPA/src/app/routes.ts
@@ -1,5 +1,5 @@
 import {Routes} from '@angular/router';
-import { MemberListComponent } from './member-list/member-list.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
 import { MessagesComponent } from './messages/messages.component';
 import { ListsComponent } from './lists/lists.component';
 import { HomeComponent } from './home/home.component';
